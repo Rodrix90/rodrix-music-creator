ecb2ab2da5dd3921fb5485c62ed6349aeeefb320
Rodrigo Gonzalez <rodryandy@gmail.com>
2026-06-09 17:25:54 -0300

    Fix de verdad: Import faltante de BackgroundTasks

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import uuid
 import tempfile
 import subprocess
 from dotenv import load_dotenv
-from fastapi import FastAPI, UploadFile, File, Form, HTTPException, Request, Depends
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException, Request, Depends, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse, FileResponse
 import json
